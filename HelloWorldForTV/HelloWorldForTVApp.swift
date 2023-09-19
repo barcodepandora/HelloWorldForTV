362c73d43f635b77f0604f4e96abe193fc394650
Juan <jmoreno@novopayment.com>
2023-09-19 13:46:14 -0500

    Stub

---
+++ HelloWorldForTV/HelloWorldForTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldForTVApp.swift
+//  HelloWorldForTV
+//
+//  Created by Juan Manuel Moreno on 23/01/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldForTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
